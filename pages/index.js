914f35a13fcc3aa09d672f3ddd2a3fdd8338cdb6
Jerome Haas <hello@jeromehaas.dev>
2022-01-05 09:18:15 +0100

    Setup: component heading

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,23 @@ import { Heading } from 'components/partials/heading/heading';
 
 const Start = () => {
   return (
-    <Heading data={{ level: 'h2', color: 'lightblue' }}>
-      Hello World!
-    </Heading>
+    <>
+      <Heading data={{ level: 'h1', color: 'lightblue' }}>
+        Diagnostik und Therapie von Kiefer-, Gesichts- und Kopfschmerzen, Myoarthropathien des Kausystems, Schlafapnoe
+      </Heading>
+      <Heading data={{ level: 'h2', color: 'darkblue' }}>
+        Ursachen für Verspannung und Migräne
+      </Heading>
+      <Heading data={{ level: 'h3', color: 'lightblue' }}>
+        Diagnose lorem ipsum dolor sit anet serum.
+      </Heading>
+      <Heading data={{ level: 'h4', color: 'lightblue' }}>
+        Termin vereinbaren
+      </Heading>
+      <Heading data={{ level: 'h5', color: 'darkblue' }}>
+        Vor der Konsultation
+      </Heading>
+    </>
   )
 }
 
