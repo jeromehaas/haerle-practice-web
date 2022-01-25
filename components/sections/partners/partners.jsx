959e2824cccfc2561e1a062c8e2110188845cb5a
Jerome Haas <hello@jeromehaas.dev>
2022-01-25 16:51:01 +0100

    Setup: component partners

---
+++ components/sections/partners/partners.jsx
@@ -0,0 +1,48 @@
+import { Section } from 'components/layouts/section/section';
+import { Paragraph } from 'components/partials/paragraph/paragraph';
+import { Heading } from 'components/partials/heading/heading';
+import { Image } from 'components/partials/image/image';
+import { Link } from 'components/partials/link/link';
+
+const Partners = ({ data }) => {
+
+  return (
+    <Section data={{ className: 'partners', anchorId: data.anchorId }}>
+      <div className="partners__text-wrapper">
+        <Heading data={{ className: 'partners__title' }}>Partner</Heading>
+        <Paragraph data={{ className: 'partners__text'}}>Mein Netzwerk deckt vielseitige Spezialisierungen ab, auf die ich jederzeit zurückgreifen kann. Davon profitieren auch Sie!</Paragraph>
+      </div>
+
+      <div className="partners__item item item--first">
+        <Image data={{ className: 'item__logo', src: '/logos/logo-zahnmedizin-luzern.png', height: '300'}} />
+        <Paragraph data={{  }}>Dr. med. dent. Jürg Eppenberger</Paragraph>
+        <Paragraph data={{  }}>Haldenstrasse 11</Paragraph>
+        <Paragraph data={{  }}>6006 Luzern</Paragraph>
+        <Paragraph data={{  }}></Paragraph>
+        <br />
+        <Paragraph data={{  }}>041 417 17 07</Paragraph>
+        <Paragraph data={{  }}>info@zahnmedizinluzern.ch</Paragraph>
+        <br />
+        <Link data={{ icon: 'external-link'}}>zahnmedizinluzern.ch</Link>
+      </div>
+
+      <div className="partners__item item item--second">
+        <Image data={{ className: 'item__logo ', src: '/logos/logo-zentrum-kopf-hals-chirurgie.png', height: '300'}} />
+        <Paragraph data={{  }}>Dr. med. dent. Jürg Eppenberger</Paragraph>
+        <Paragraph data={{  }}>Haldenstrasse 11</Paragraph>
+        <Paragraph data={{  }}>6006 Luzern</Paragraph>
+        <br />
+        <Paragraph data={{  }}></Paragraph>
+        <Paragraph data={{  }}>041 417 17 07</Paragraph>
+        <Paragraph data={{  }}>info@zahnmedizinluzern.ch</Paragraph>
+        <br />
+        <Link data={{ icon: 'external-link'}}>kopf-hals-chierurgie.ch</Link>
+      </div>
+    </Section>
+  )
+
+}
+
+export {
+  Partners
+};
--- pages/aktivitaeten.js
+++ pages/aktivitaeten.js
@@ -2,6 +2,7 @@ import { Page } from 'components/layouts/page/page';
 import { Intro } from 'components/sections/intro/intro';
 import { CTA } from 'components/sections/cta/cta';
 import { Education } from 'components/sections/education/education';
+import { Partners } from 'components/sections/partners/partners';
 
 const Aktivitaeten = () => {
 
@@ -18,6 +19,7 @@ const Aktivitaeten = () => {
         ]
       }}/>
       <Education data={{ anchorId: 'weiterbildungen-tagungen'}} />
+      <Partners data={{ anchorId: 'partner' }}></Partners>
       <CTA data={{
         title: 'Termin vereinbaren',
         text: 'Sie finden mich in der Zahnmedizin Luzern AG, bei Dr. Jürg Eppenberger, Zahnarztpraxis für Rekonstruktive Zahnmedizin.',
