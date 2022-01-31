ef0d234b8cef8923389b059c706aee2ed8e1dd82
Jerome Haas <hello@jeromehaas.dev>
2022-01-31 17:17:23 +0100

    Cleanup: component resume

--- components/sections/resume/resume.jsx
+++ components/sections/resume/resume.jsx
@@ -6,7 +6,7 @@ import { Link } from 'components/partials/link/link';
 
 const Resume = ({ data }) => {
   return (
-    <Section data={{ className: 'faq', anchorId: data.anchorId }}>
+    <Section data={{ className: 'resume', anchorId: data.anchorId }}>
       <div className="resume__wrapper">
 
         <Heading data={{ className: 'resume__heading', level: 'h2' }}>Lebenslauf</Heading>
@@ -29,19 +29,19 @@ const ResumeItem = ({ data }) => {
 
 
   return (
-    <div className="resume__item">
-      <Heading data={{  className: 'resume__item-date', level: 'h3', color: 'lightblue' }}>
+    <div className="resume__item item">
+      <Heading data={{  className: 'item__date', level: 'h3', color: 'darkblue' }}>
         { data.date }
       </Heading>
-      <div data={{ className: 'resume__item-activity' }}>
+      <div data={{ className: 'item__activity' }}>
         { data.activities && data.activities.map((activity, index) => (
           <Paragraph data={{}} key={ index }>{activity}</Paragraph>
         ))}
       </div>
       { data.links && (
-        <div className='resume__item-links'>
+        <div className='item__links'>
           { data.links && data.links.map((link, index) => (
-            <Link key={ index } data={{ className: 'resume__item-link', href: link.link, icon: 'pdf', target: '_blank' }}>{ link.text }</Link> 
+            <Link key={ index } data={{ className: 'item__link', href: link.link, icon: 'pdf', target: '_blank' }}>{ link.text }</Link> 
           ))}
         </div>
       ) }
