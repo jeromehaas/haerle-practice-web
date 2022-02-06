ef17b495c7bbfb80ee407b83fe2690f45f302af1
Jerome Haas <hello@jeromehaas.dev>
2022-02-06 14:03:31 +0100

    Fix: linter errors

--- components/sections/hero/hero.jsx
+++ components/sections/hero/hero.jsx
@@ -1,7 +1,6 @@
 import { Section } from 'components/layouts/section/section';
 import { Image } from 'components/partials/image/image';
 import { Heading } from 'components/partials/heading/heading';
-import { Icon } from 'components/partials/icon/icon';
 import { Link } from 'react-scroll';
 
 const Hero = ({ data }) => {
