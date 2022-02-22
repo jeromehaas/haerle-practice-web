c0c7f48a170c36db677b680db10b4b5090a5bba5
Jerome Haas <hello@jeromehaas.dev>
2022-02-22 10:47:40 +0100

    Refactor: remove points in titles

--- pages/schmerztherapie.js
+++ pages/schmerztherapie.js
@@ -24,12 +24,12 @@ const Schmerztherapie = () => {
       }}/>
       <Symptoms data={{
         anchorId: 'symptome',
-        title: 'Vielseitige Beschwerden, ganzheitliche Betrachtung.',
+        title: 'Vielseitige Beschwerden, ganzheitliche Betrachtung',
         text: 'Eine Cranio Mandibulären Dysfunktion (CMD) führt zu Schmerzen und/oder Funktionseinschränkungen. Sofern Beschwerden auftreten, kann sich das durch verschiedene ein- oder beidseitige Symptome äussern, etwa durch Schmerzen im Kiefer-, Gesichts- und Nackenbereich, Schlafprobleme oder unklare Zahnschmerzen. Zur Abklärung der verschiedenen Symptome, wann diese erstmals auftraten, ob es Begleiterscheinungen gibt und welche Therapien Sie diesbezüglich schon unternommen haben, senden wir Ihnen vor der ersten Konsultation einen Fragebogen zu. Beim Besuch in der Praxis folgt eine Messung der Unterkieferbeweglichkeit, ein Untersuch der Muskulatur, der Gelenke und des Bisses. Es werden erste Therapieansätze besprochen.'
       }} />
       <Article data={{
         anchorId: 'diagnose',
-        title: 'Die Diagnose ist so individuell wie Sie.',
+        title: 'Die Diagnose ist so individuell wie Sie',
         text: 'Nach der zweiten Sitzung ist meist klar welche Richtung für die Therapie eingeschlagen wird. Vieles werden wir in ein, zwei weiteren Behandlungsterminen selber angehen und bewältigen können. Manchmal sind aber auch zusätzliche Abklärungen (z.B. in einer Schlafklinik, oder HNO-Arzt) oder Therapieansätze bei weiteren Fachpersonen (Neurologie, Rheumatologie, Psychologie, Physiotherapie, Akupunktur, integrative Medizin u.a.) sinnvoll. Da ich über ein gutes Netzwerk zu solchen Personen verfüge kann Ihnen ein interdisziplinäres Setting angeboten werden. ',
         imageSrc: '/images/07-daniela-haerle-besprechung-roentgen-ipad.jpg',
         imageAlt: '#',
@@ -37,7 +37,7 @@ const Schmerztherapie = () => {
       }} /> 
       <Article data={{
         anchorId: 'therapie',
-        title: 'Die auf Sie zugeschnittene Therapie. ',
+        title: 'Die auf Sie zugeschnittene Therapie',
         text: 'Die Therapie beginnt als Erstes bei Ihnen im Kopf. Sie werden Zusammenhänge besser verstehen und ungünstige Gewohnheiten ablegen können. Es kommen reversible Therapiehilfsmittel wie orale Schienen (Entlastungsschiene, Schnarchschiene, Protrusionsschiene) zum Einsatz. Auch helfen gezielte lokale Anästhesien und das Erlernen von einfacher Physiotherapie für den Kiefer. Spezifische Medikamente können Linderung verschaffen. Sollte sich eine Fehlstellung des Bisses oder ein Zahn als Ursache der Schmerzen herausstellen, stehe ich gerne beratend Ihrem Privat-Zahnarzt zur Seite (second opinion). ',
         imageSrc: '/images/08-daniela-haerle-besprechung-patient-closeup.jpg',
         imageAlt: '#',
