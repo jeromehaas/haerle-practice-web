21959ca9e3d328db13969d4a5595b2a3b84a3ab5
Jerome Haas <hello@jeromehaas.dev>
2022-02-22 10:38:52 +0100

    Refactor: add link for eaopd

--- components/sections/memberships/memberships.jsx
+++ components/sections/memberships/memberships.jsx
@@ -12,11 +12,11 @@ const Memberships = ({ data }) => {
         <Paragraph data={{ className: 'memberships__text'}}>Frau Dr. med. dent. Daniela Haerle ist Mitglied von anerkannten Fachgesellschaften; des Weiteren besteht eine Zusammenarbeit mit dem Zentrum für Kopf-Hals-Chirurgie Hirslanden Klinik St. Anna, spezialisierte Zentren für Schlafmedizin, HNO-Ärzten, Neurologen und Psychologen.</Paragraph>
       </div>
       <div className="memberships__logo-wrapper">
-        <a className="memberships__logo memberships__logo--1" href="https://swisspainsociety.ch/de/" target="_blank" rel="noreferrer"><Image data={{ src: '/logos/logo-swiss-pain-society.png'}} /></a>
-        <a className="memberships__logo memberships__logo--2" href="https://www.sso-luzern.ch/" target="_blank" rel="noreferrer"><Image data={{ src: '/logos/logo-zbk-lzg.png'}} /></a>
-        <a className="memberships__logo memberships__logo--3" href="https://www.sso.ch/" target="_blank" rel="noreferrer"><Image data={{ src: '/logos/logo-sso.png'}} /></a>
-        <a className="memberships__logo memberships__logo--4" href="https://www.dgfdt.de/" target="_blank" rel="noreferrer"><Image data={{ src: '/logos/logo-dgfdt.png'}} /></a>
-        <Image data={{ className: 'memberships__logo memberships__logo--5', src: '/logos/logo-eaopd.png'}} />
+        <a className="memberships__logo memberships__logo--1" href="https://swisspainsociety.ch/de/" target="_blank" ><Image data={{ src: '/logos/logo-swiss-pain-society.png'}} /></a>
+        <a className="memberships__logo memberships__logo--2" href="https://www.sso-luzern.ch/" target="_blank" ><Image data={{ src: '/logos/logo-zbk-lzg.png'}} /></a>
+        <a className="memberships__logo memberships__logo--3" href="https://www.sso.ch/" target="_blank" ><Image data={{ src: '/logos/logo-sso.png'}} /></a>
+        <a className="memberships__logo memberships__logo--4" href="https://www.dgfdt.de/" target="_blank" ><Image data={{ src: '/logos/logo-dgfdt.png'}} /></a>
+        <a className="memberships__logo memberships__logo--5" href="https://www.eaopd.org" target="_blank" ><Image data={{ src: '/logos/logo-eaopd.png'}} /></a>
       </div>
     </Section>
   )
